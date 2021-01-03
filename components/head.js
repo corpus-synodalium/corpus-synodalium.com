3e0e2e5814feab310281ebe3bed73677fb01c8b0
Thawsitt Naing <thawsitt@gmail.com>
2021-01-03 12:35:48 -0800

    Fix console error

--- components/head.js
+++ components/head.js
@@ -36,7 +36,7 @@ export const DefaultHead = () => (
                             window.dataLayer = window.dataLayer || [];
                             function gtag(){dataLayer.push(arguments);}
                             gtag('js', new Date());
-                            gtag('config', ${GA_ID});
+                            gtag('config', '${GA_ID}');
                         `,
             }}
         />
