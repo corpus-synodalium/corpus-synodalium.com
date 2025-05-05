fc0d0bb86b79eee19e178c7cdf8c8cb9cb1ec9e0
Thawsitt Naing <thawsitt@gmail.com>
2025-05-04 18:27:31 -0700

    Update publications

--- pages/publications.js
+++ pages/publications.js
@@ -13,13 +13,14 @@ const Publications = () => (
         </p>
         <ul className="publications-list">
             <li>
-                Allegria, Simone. “Costituzioni sinodali diocesane e reclusione
+                Allegria, Simone. "Costituzioni sinodali diocesane e reclusione
                 volontaria in Italia centrale tra Due e Trecento. Qualche
-                riflessione.” <i>Quaderni di storia religiosa medievale</i> 24/1
+                riflessione." <i>Quaderni di storia religiosa medievale</i> 24/1
                 (2021): 143-165. [
                 <a
                     href="https://www.rivisteweb.it/doi/10.32052/101640"
                     target="_blank"
+                    rel="noopener noreferrer"
                 >
                     link
                 </a>
@@ -27,55 +28,61 @@ const Publications = () => (
             </li>
 
             <li>
-                Avignon, Carole. “Fiançailles, juridictions et disciplines
-                synodales. Ambiguïtés d’une norme et discontinuités de sa
-                diffusion (13e-15e siècle).” <i>Revue de droit canonique</i>{' '}
+                Arnold, John H.{' '}
+                <i>
+                    The Making of Lay Religion in Southern France, c. 1000-1350
+                </i>
+                . Oxford: Oxford University Press, 2024.
+            </li>
+
+            <li>
+                Avignon, Carole. "Fiançailles, juridictions et disciplines
+                synodales. Ambiguïtés d'une norme et discontinuités de sa
+                diffusion (13e-15e siècle)." <i>Revue de droit canonique</i>{' '}
                 71/1 (2021): 109-138.
             </li>
 
             <li>
-                Baker, Lane. “The Sound of Sin: Episcopal Noise Regulation in
-                the Later Middle Ages.” <i>Viator</i> (forthcoming).
+                Baker, Lane. "The Sound of Sin: Episcopal Noise Regulation in
+                the Later Middle Ages." <i>Viator</i> 52, no. 2 (2021): 131-178.
             </li>
 
             <li>
-                Beaulande-Barraud, Véronique. “Le synodal de Reims en sa
+                Beaulande-Barraud, Véronique. "Le synodal de Reims en sa
                 province (des statuts de Guiard de Laon au synodal de Guillaume
-                de Trie).” <i>Revue de droit canonique</i> 71/1 (2021): 21-38.
+                de Trie)." <i>Revue de droit canonique</i> 71/1 (2021): 21-38.
             </li>
 
             <li>
-                Dorin, Rowan. “The Bishop as Lawmaker in Late Medieval Europe.”
+                Dorin, Rowan. "The Bishop as Lawmaker in Late Medieval Europe."
                 <i>Past and Present</i> 253 (Nov. 2021): 45-82. [
-                <a href="https://doi.org/10.1093/pastj/gtaa045" target="_blank">
+                <a
+                    href="https://doi.org/10.1093/pastj/gtaa045"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
                     link
                 </a>
                 ]
             </li>
 
             <li>
-                Dorin, Rowan. “Corpus Synodalium: Medieval Canon Law in the
-                Digital Age.” In{' '}
+                Dorin, Rowan. "<i>Corpus Synodalium</i>: Medieval Canon Law in
+                the Digital Age." In{' '}
                 <i>Digital Medieval Studies—Practice and Preservation</i>, eds.
                 Laura Morreale and Sean Gilsdorf, 49-75. Leeds: ARC Humanities
                 Press, 2022.
             </li>
 
             <li>
-                Dorin, Rowan, and Raffaella Bianchi Riva. “Usury and Restitution
+                Dorin, Rowan, and Raffaella Bianchi Riva. "Usury and Restitution
                 in Late Medieval Episcopal Statutes: A Case Study in the Local
-                Reception of Conciliar Decrees.”{' '}
-                <i>Bulletin of Medieval Canon Law</i>
-                38 (2021): 309-359.
-            </li>
-
-            <li>
-                Doten-Snitker, Kerice. “Medieval Political Competition and the
-                Attack on Ethnoreligious Diversity.” PhD Dissertation,
-                University of Washington, 2020. [
+                Reception of Conciliar Decrees."{' '}
+                <i>Bulletin of Medieval Canon Law</i> 38 (2021): 309-359. [
                 <a
-                    href="https://digital.lib.washington.edu/researchworks/bitstream/handle/1773/46194/DotenSnitker_washington_0250E_21659.pdf?sequence=1&isAllowed=y"
+                    href="http://legalhistorysources.com/BMCL-38-2021.pdf"
                     target="_blank"
+                    rel="noopener noreferrer"
                 >
                     link
                 </a>
@@ -83,33 +90,83 @@ const Publications = () => (
             </li>
 
             <li>
-                Fossier, Arnaud. “Confession et absolution dans les statuts
-                synodaux français et italiens (13e-15e siècle).”{' '}
+                Doten-Snitker, Kerice. "Medieval Political Competition and the
+                Attack on Ethnoreligious Diversity." PhD Dissertation,
+                University of Washington, 2020. [
+                <a
+                    href="https://digital.lib.washington.edu/researchworks/bitstream/handle/1773/46194/DotenSnitker_washington_0250E_21659.pdf?sequence=1&isAllowed=y"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    link
+                </a>
+                ]
+            </li>
+
+            <li>
+                Fossier, Arnaud. "Confession et absolution dans les statuts
+                synodaux français et italiens (13e-15e siècle)."{' '}
                 <i>Revue de droit canonique</i> 71/1 (2021): 139-174.
             </li>
 
             <li>
-                Lusset, Élisabeth. “Les statuts synodaux bilingues du diocèse de
-                Troyes (14e-15e siècle.” <i>Revue de droit canonique</i> 71/1
+                Freidenreich, David.{' '}
+                <i>
+                    Jewish Muslims: How Christians Imagined Islam as the Enemy
+                </i>
+                . Oakland: University of California Press, 2022.
+            </li>
+
+            <li>
+                Fudge, Thomas A.{' '}
+                <i>
+                    Coerced Ordinations in the Late Medieval Bohemian Province
+                </i>
+                . London: Lexington Books, 2023.
+            </li>
+
+            <li>
+                Kirkland, Justin S. "The 1444 Sunday law of London."{' '}
+                <i>Historical Research</i> 97 (2024): 295-306. [
+                <a
+                    href="https://academic.oup.com/histres/advance-article/doi/10.1093/hisres/htae002/7632081"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    link
+                </a>
+                ]
+            </li>
+
+            <li>
+                Lusset, Élisabeth. "Les statuts synodaux bilingues du diocèse de
+                Troyes (14e-15e siècle." <i>Revue de droit canonique</i> 71/1
                 (2021): 39-68.
             </li>
 
             <li>
-                Pagnoni, Fabrizio. “Coutume, droit canonique, pouvoir épiscopal.
+                Morreale, Laura, et al. "Digital Scholarship in the Field of
+                Church History." <i>Catholic Historical Review</i> 109 (2023):
+                132-150.
+            </li>
+
+            <li>
+                Pagnoni, Fabrizio. "Coutume, droit canonique, pouvoir épiscopal.
                 La diffusion des normes relatives aux dîmes dans les
-                législations synodales (Italie du Nord, 13e-14e siècle).”{' '}
+                législations synodales (Italie du Nord, 13e-14e siècle)."{' '}
                 <i>Revue de droit canonique</i> 71/1 (2021): 175-206.
             </li>
 
             <li>
-                Perron, Anthony. “Scandal of the Church, Prison of the Soul. The
+                Perron, Anthony. "Scandal of the Church, Prison of the Soul. The
                 Problem of Bad Custom in Twelfth- and Thirteenth-Century Canon
-                Law and Practice.”{' '}
+                Law and Practice."{' '}
                 <i>Historical Reflections/Réflexions Historiques</i> 47/3
                 (2021): 20-38. [
                 <a
                     href="https://doi.org/10.3167/hrrh.2021.470303"
                     target="_blank"
+                    rel="noopener noreferrer"
                 >
                     link
                 </a>
@@ -117,13 +174,34 @@ const Publications = () => (
             </li>
 
             <li>
-                Somogyi, Szilvia. “Az esztergomi szinodális könyvről: új
+                Righi, Laura. "Amministrare l'usura. La diffusione
+                dell'interesse nella canonistica e nella legislazione medievale
+                (XII-XIV secolo)."
+                <i>Cristianesimo nella storia</i> 44/1 (2023): 31-57.
+            </li>
+
+            <li>
+                Righi, Laura. "Digital Dark Ages: The Role of Medieval Corpora
+                in the Context of the Digital Humanities and Religious Studies."{' '}
+                <i>Cristianesimo nella storia</i> 44/3 (2023): 837-852.
+            </li>
+
+            <li>
+                Schmugge, Ludwig. "Self-Defense in Fifteenth-Century Church
+                Courts: The Requirement of Acting 'with the Moderation of Lawful
+                Protection'(cum moderamine inculpate tutele)."{' '}
+                <i>Bulletin of Medieval Canon Law</i> n.s. 40 (2023): 157-172.
+            </li>
+
+            <li>
+                Somogyi, Szilvia. "Az esztergomi szinodális könyvről: új
                 források, redakciók és szövegtöbbletek nyomában. (Előtanulmány
-                egy disszertációfejezethez).” <i>Magyar Könyvszemle</i> 135
+                egy disszertációfejezethez)." <i>Magyar Könyvszemle</i> 135
                 (2019): 1-17. [
                 <a
                     href="http://real.mtak.hu/102885/1/SomogyiSz_MKsz_19-1.pdf"
                     target="_blank"
+                    rel="noopener noreferrer"
                 >
                     link
                 </a>
@@ -131,9 +209,36 @@ const Publications = () => (
             </li>
 
             <li>
-                Somogyi, Szilvia. “Das spätmittelalterliche Synodalbuch der
-                Diözese von Gran.” <i>Annales Historiae Conciliorum</i> 50, no.
+                Somogyi, Szilvia. "Das spätmittelalterliche Synodalbuch der
+                Diözese von Gran." <i>Annales Historiae Conciliorum</i> 50, no.
                 2 (2020): 395-416.
+            </li>
+
+            <li>
+                Stow, Kenneth R.{' '}
+                <i>
+                    Feeding the Eternal City: Jewish and Christian Butchers in
+                    the Roman Ghetto
+                </i>
+                . Cambridge, MA: Harvard University Press, 2024.
+            </li>
+
+            <li>
+                West, Charles. "The Earliest Form and Function of the 'Admonitio
+                synodalis'." <i>Frühmittelalterliche Studien</i> 57, no. 1
+                (2023): 347-380.
+            </li>
+
+            <li>
+                Woelki, Thomas. "Die fürsorgliche Kirche? Kinderschutz und{' '}
+                <i>oppressio puerorum</i> in mittelalterlichen Synodalstatuten."
+                In{' '}
+                <i>
+                    Synodalis consonantia. Konziliengeschichte als Spiegelbild
+                    kirchlicher Diskussionskultur
+                </i>
+                , ed. Filippo Forlani et al., 207-222. Münster: Aschendorff,
+                2024.
             </li>
         </ul>
     </>
